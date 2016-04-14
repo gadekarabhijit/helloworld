9c65f6eb349df543caab8fd09d0d15daa3cbc786
Abhijit Gadekar <abhijitg712@gmail.com>
2016-04-14 23:08:18 +0530

    Added configuration

--- src/main/java/com/dreamland/sample/Application.java
+++ src/main/java/com/dreamland/sample/Application.java
@@ -1,6 +1,6 @@
 package com.dreamland.sample;
 
-import org.springframework.boot.SpringApplication;
+/*import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,17 +9,16 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-
 @Configuration
 @ComponentScan
 @EnableJpaRepositories
 @Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
-@PropertySource("application.properties")
+@PropertySource("application.properties")*/
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);			
+		//SpringApplication.run(Application.class, args);			
 	}
 
 }
